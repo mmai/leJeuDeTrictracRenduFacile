d4eb57c50499889be3582bcffad8fc71d7934525
Henri Bourcereau <henri.bourcereau@gmail.com>
2022-12-06 21:05:12 +0100

    fix epub images

--- makeEpub.sh
+++ makeEpub.sh
@@ -1,7 +1,24 @@
 #!/bin/sh
 
-cd src
-pandoc -o trictracFacile.epub metadata.yaml \
+FILENAME=trictracFacile
+
+rm -rf src_epub
+cp -r src src_epub
+cd src_epub
+
+for file in ./*.md 
+do
+  sed -i 's/\.svg/.png/' $file
+done
+
+for img in ./*.svg 
+do
+  echo "Converting $img"
+  convert $img "${img%.svg}.png" 
+  rm $img
+done
+
+pandoc -o $FILENAME.epub metadata.yaml \
    index.md \
    preface.md \
    avertissement.md \
@@ -11,4 +28,7 @@ pandoc -o trictracFacile.epub metadata.yaml \
    section3.md \
    section4.md \
    section5.md
-echo "file ready in ./src/trictracFacile.epub"
+cd ..
+cp src_epub/$FILENAME.epub src/
+rm -rf src_epub
+echo "file ready in ./src/$FILENAME.epub"
